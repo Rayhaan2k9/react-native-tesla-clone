5644a26427204afa4f1ed3335bb8d7cef5e60689
Rayhaan Ugharadar <Rayhaan@Mohameds-MacBook-Pro.local>
2022-02-18 15:06:40 +0000

    rendered buttons on page and styled

--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -1,6 +1,7 @@
 import react from "react";
 import { View, Text, ImageBackground } from "react-native";
 import { styles } from "./styles";
+import { StyledButton } from "../StyledButton";
 
 export const CarItem = () => {
     return (
@@ -15,6 +16,18 @@ export const CarItem = () => {
           <Text style={styles.heading}>Model S</Text>
           <Text style={styles.subheading}>Starting at £60,000</Text>
         </View>
+
+        <StyledButton type='primary'
+         content={'CUSTOM ORDER'}
+         onPress={() => {
+           console.log('Custom button pressed')
+         }} />
+
+        <StyledButton type='secondary'
+         content={'EXISTING INVENTORY'}
+         onPress={() => {
+           console.log('Existing order pressed')
+         }} />
 
       </View>
     )
